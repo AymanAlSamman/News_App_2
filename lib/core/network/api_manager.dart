a6b6eac215ebb2dd6c04ddc8b0a5ee0f05b6cf81
mac <ayman.emad.mo@gmail.copm>
2024-03-19 06:46:34 -0700

    Api Manager Step 2 (Get sources)

--- lib/core/network/api_manager.dart
+++ lib/core/network/api_manager.dart
@@ -1,16 +1,31 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:news_app_2/core/config/constants.dart';
 import 'package:news_app_2/models/category_model.dart';
+import 'package:news_app_2/models/source_model.dart';
 
 class ApiManager {
-  static fetchDataSources(String categoryId) async  {
+
+  static Future<List<SourceModel>> fetchDataSources(String categoryId) async  {
     var url = Uri.https(Constants.baseUrl, "/v2/top-headlines/sources", {
       "apiKey": Constants.apiKey,
       "category": categoryId,
     });
    final response = await http.get(url);
+   List<SourceModel> sourcesDataList = [];
    if(response.statusCode == 200){
      print(response.body);
+      var data = jsonDecode(response.body);
+      var sourcesList = data["sources"] as List;
+
+      for(var element in sourcesList){
+        sourcesList.add( SourceModel.fromJson(element));
+      }
+      return sourcesDataList;
+   } else {
+
+     throw Exception('Failed to load album');
    }
   }
 }
--- lib/home/page/category_view.dart
+++ lib/home/page/category_view.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_2/core/config/constants.dart';
 import 'package:news_app_2/core/network/api_manager.dart';
 import 'package:news_app_2/core/widget/custom_background_widget.dart';
 import 'package:news_app_2/models/category_model.dart';
 
 class CategoryView extends StatelessWidget {
   final CategoryModel categoryModel;
+
   const CategoryView({super.key, required this.categoryModel});
 
   @override
   Widget build(BuildContext context) {
-    ApiManager.fetchDataSources(categoryModel.id);
     return CustomBackgroundWidget(
-child: Container(),
+      child: FutureBuilder(
+        future: ApiManager.fetchDataSources(categoryModel.id),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("same thing went wrong"),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var sourcesList = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemBuilder: (context, index) => Text(
+              sourcesList[index].name,
+              style: Constants.theme.textTheme.bodyLarge?.copyWith(
+                color: Colors.black,
+              ),
+            ),
+            itemCount: sourcesList.length,
+          );
+        },
+      ),
     );
   }
 }
