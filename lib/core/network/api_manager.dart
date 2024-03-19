edda1f692408b473204fa6e76f189edcd4132d03
mac <ayman.emad.mo@gmail.copm>
2024-03-19 03:11:00 -0700

    Api Manager Step 1

---
+++ lib/core/network/api_manager.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'package:news_app_2/core/config/constants.dart';
+import 'package:news_app_2/models/category_model.dart';
+
+class ApiManager {
+  static fetchDataSources (String categoryId ) async  {
+
+    var url  = Uri.https(
+      Constants.apiKey,
+      "v2/top-headlines/sources",
+      {
+         "apiKey" : Constants.apiKey,
+        "category" : categoryId,
+      }
+    );
+
+    final response = await http.get(url);
+
+    if(response.statusCode == 200){
+      print(response.body);
+    }
+  }
+}
--- lib/home/page/category_view.dart
+++ lib/home/page/category_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_2/core/network/api_manager.dart';
 import 'package:news_app_2/core/widget/custom_background_widget.dart';
 import 'package:news_app_2/models/category_model.dart';
 
@@ -8,6 +9,7 @@ class CategoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ApiManager.fetchDataSources(categoryModel.id);
     return CustomBackgroundWidget(
 
     );
