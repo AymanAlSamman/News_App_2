35da32a5d8035a5f6f7cce05718da82c21ded186
mac <ayman.emad.mo@gmail.copm>
2024-03-19 05:21:54 -0700

    Api Manager Step 1

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -6,6 +6,10 @@ class Constants {
       MediaQuery.of(navigatorKey.currentState!.context).size;
   static var theme = Theme.of(navigatorKey.currentState!.context);
 
+
   static const String apiKey = "e6156b42aff94100b32f6983d38d1887";
   static const String baseUrl = "newsapi.org";
+
 }
+
+
--- lib/core/network/api_manager.dart
+++ lib/core/network/api_manager.dart
@@ -3,21 +3,14 @@ import 'package:news_app_2/core/config/constants.dart';
 import 'package:news_app_2/models/category_model.dart';
 
 class ApiManager {
-  static fetchDataSources (String categoryId ) async  {
-
-    var url  = Uri.https(
-      Constants.apiKey,
-      "v2/top-headlines/sources",
-      {
-         "apiKey" : Constants.apiKey,
-        "category" : categoryId,
-      }
-    );
-
-    final response = await http.get(url);
-
-    if(response.statusCode == 200){
-      print(response.body);
-    }
+  static fetchDataSources(String categoryId) async  {
+    var url = Uri.https(Constants.baseUrl, "/v2/top-headlines/sources", {
+      "apiKey": Constants.apiKey,
+      "category": categoryId,
+    });
+   final response = await http.get(url);
+   if(response.statusCode == 200){
+     print(response.body);
+   }
   }
 }
